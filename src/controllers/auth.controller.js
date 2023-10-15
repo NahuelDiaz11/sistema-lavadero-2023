49df61d3b2f690a17ac2848eae877fbe1e94dd93
nahueldiaz23 <nahuel.diaz@syloper.com>
2023-10-15 15:25:24 -0300

    feat: create user registration and token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,20 +1,33 @@
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import { createAccesToken } from '../libs/jwt.js';
 
 const prisma = new PrismaClient();
 export const register = async (req, res) => {
     try {
+
       const { nombre, apellido, email, pass } = req.body;
+      const passwordHash = await bcrypt.hash(pass, 5);
   
       const newUser = await prisma.usuarios.create({
         data: {
           nombre,
           apellido,
           email,
-          pass,
+          pass: passwordHash,
         },
       });
-  
-      res.json({ message: 'User successfully registered', user: newUser });
+      
+      const userSaved = newUser;
+      const token = await createAccesToken({ id: userSaved.id });
+
+      res.cookie('token', token)
+   
+      res.json({ 
+        message: 'User successfully registered',
+        email: userSaved.email,
+        nombre: userSaved.nombre,
+     });
     } catch (error) {
       console.error('Error registering user:', error);
       res.status(500).json({ error: 'Error registering user' });
