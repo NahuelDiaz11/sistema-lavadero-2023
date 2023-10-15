8b04d9217ae06d8fac56724c5cc5a85b3f6e9ced
nahueldiaz23 <nahuel.diaz@syloper.com>
2023-10-14 23:00:05 -0300

    feature: add register of user and model user

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ import express from 'express';
 import morgan from 'morgan';
 import authRoutes from './routes/auth.routes.js';
 
-const app = express()
+const app = express();
+
 app.use(morgan('dev'));
 
-app.use(authRoutes);
+app.use(express.json());
+
+app.use("/api", authRoutes);
 
 export default app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,2 +1,24 @@
-export const register = (req, res) => res.send('register');
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+export const register = async (req, res) => {
+    try {
+      const { nombre, apellido, email, pass } = req.body;
+  
+      const newUser = await prisma.usuarios.create({
+        data: {
+          nombre,
+          apellido,
+          email,
+          pass,
+        },
+      });
+  
+      res.json({ message: 'User successfully registered', user: newUser });
+    } catch (error) {
+      console.error('Error registering user:', error);
+      res.status(500).json({ error: 'Error registering user' });
+    }
+  };
+
 export const login = (req, res) => res.send('login');
