f03d54d1b05bf8b3dddae09000d39ab91a516990
nahueldiaz23 <nahuel.diaz@syloper.com>
2023-10-14 21:18:00 -0300

    first commit

---
+++ src/db.js
@@ -0,0 +1,18 @@
+import mysql from 'mysql';
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'secret',
+  database: 'lavadero'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Connection error: ' + err.stack);
+    return;
+  }
+  console.log('Connected to the identifier' + connection.threadId);
+});
+
+export default connection;
